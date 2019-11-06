a77e33ba692994ac01849176526326fce5aa2abb
nazkin <nazar.kinash93@gmail.com>
2019-11-06 15:11:38 -0500

    User input captured using DOM manipulation, all there is to do is making an init function and to launch code through button instead of console

--- script.js
+++ script.js
@@ -1,45 +1,86 @@
 //Create data structures for each of the user selection eg(lower-case letters, numericals etc)
 
+
+
+//DATA
+
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var specialCharacters = "!@#$%^&*?";
 
-var passwordlength = 20; // User defined length
-var passwordStr = ""; // The string that will contain the total of all characters used to generate password
+var passwordStr; // The string that will contain the total of all characters used to generate password
+var generatedPassword; //The string password that will be automatically generated
+var passwordLength; //length of password
 
-var generatedPassword = ""; //The string password that will be automatically generated
+var wantUpper;
+var wantLower ;
+var wantNumber ;
+var wantSpecial ;
+
+function initForm() {
+    wantUpper = false;
+    wantLower = false;
+    wantNumber = false;
+    wantSpecial = false;
+    passwordLength = 1;
+    generatedPassword = "";
+    passwordStr = "";
+    
+}
+
+initForm();
+  
 
 //Boolean variables that will be corresponding to the radio selections in the html
-var wantUpper = true; 
-var wantLower = true;
-var wantNumber = true;
-var wantSpecial = false;
+
+
+
+//Create DOM querySelectors to capture the input from chackboxes
+
+wantUpper = document.querySelector('#largeL').checked;
+wantLower = document.querySelector('#smallL').checked;
+wantNumber = document.querySelector('#numL').checked;
+wantSpecial = document.querySelector('#specialL').checked;
+//same as passwordLength variabl test
+passwordLength = document.querySelector('.userLengthInput').value;
+
 
 //Conditional control of radio selections to control the content of generated password 
-if(wantUpper == true){
-  passwordStr = passwordStr.concat(upperCase);
-}
-
-if(wantLower == true){
-    passwordStr = passwordStr.concat(lowerCase);
-}
-
-if(wantNumber == true){
-    passwordStr = passwordStr.concat(numbers);
-}
-
-if(wantSpecial == true){
-    passwordStr = passwordStr.concat(specialCharacters);
-}
-
-console.log("Your sample is " + passwordStr);
-
-//Create a random function that selects a random character from the final array 
-for(var x = 0; x < passwordlength; x++) {
-    generatedPassword += passwordStr.charAt(Math.floor(Math.random() * passwordStr.length)); 
-}
-console.log("Your password is " + generatedPassword);
+console.log(wantUpper);
+ 
 
 
-//Find a way to munipulate the DOM to read input from the form elements and output my password
+  
+
+    if(wantUpper == true){
+        passwordStr = passwordStr.concat(upperCase);
+      }
+      
+      if(wantLower == true){
+          passwordStr = passwordStr.concat(lowerCase);
+      }
+      
+      if(wantNumber == true){
+          passwordStr = passwordStr.concat(numbers);
+      }
+      
+      if(wantSpecial == true){
+          passwordStr = passwordStr.concat(specialCharacters);
+      }
+
+      if(wantSpecial === false && wantNumber === false && wantLower === false && wantUpper === false){
+          console.log ("You must specify at least 1 group of characters to build a password")
+      }
+       
+      console.log("Your sample is " +  passwordStr);
+       
+      //Create a random function that  selects a random character from the final array 
+      for(var x = 0; x < passwordLength; x++) {
+          generatedPassword += passwordStr.charAt(Math.floor(Math.random() * passwordStr.length)); 
+      }
+      console.log("Your password is " + generatedPassword);
+      
+      
+
+
